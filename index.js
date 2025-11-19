74ab56bca2ac8922b8d27284c2e784546d1c53d8
Midengrows1 <dev05@personhunters.com>
2025-11-19 22:20:14 +0300

    feat: add conditional rendering for navigation

--- index.js
+++ index.js
@@ -12,10 +12,24 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 //*
-
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Главная ",
+    isHome: true,
+  });
+});
+
+app.get("/courses", (req, res) => {
+  res.render("courses", {
+    title: "Курсы",
+    isCourses: true,
+  });
+});
+app.get("/add", (req, res) => {
+  res.render("add", {
+    title: "Добавить Курсы",
+    isAdd: true,
   });
 });
 
