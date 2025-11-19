f7d206da59d9a470dc12a19e0578ff5270d221dc
Midengrows1 <dev05@personhunters.com>
2025-11-19 21:54:16 +0300

    fix conflicts

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const exhbs = require("express-handlebars");
+
+const hbs = exhbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+//* Configuring Handlebars
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+//*
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Application started at https://localhost:${PORT}`);
+});
