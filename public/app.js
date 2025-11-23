bab2f6c1fa5542ecdc2235cd73ccd9bd5b444b8f
Midengrows1 <dev05@personhunters.com>
2025-11-23 22:00:56 +0300

    feat: add  deletion of the courses from  the cart page

--- public/app.js
+++ public/app.js
@@ -5,3 +5,19 @@ document.querySelectorAll(".price").forEach((node) => {
     maximumSignificantDigits: 3,
   }).format(node.textContent);
 });
+
+const $cardCourse = document.querySelector(".card-course");
+$cardCourse.addEventListener("click", (event) => {
+  if (event.target.classList.contains("js-remove")) {
+    const id = event.target.dataset.id;
+    console.log(id);
+
+    fetch("/card/remove/" + id, {
+      method: "delete",
+    })
+      .then((res) => res.json())
+      .then((card) => {
+        console.log(card);
+      });
+  }
+});
